5d831dbb6ec981b125b1910c18b01200ba402f01
Rowan <rowjw97@gmail.com>
2024-01-26 18:58:21 +0000

    Set up of component file structure.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import SearchBar from './components/SearchBar';
+import SearchResults from './components/SearchResults';
+import Playlist from './components/Playlist';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className='banner'>
+        <h1>Jammming</h1>
+      </div>
+      <div className='double-column'>
+        <div className='search-container'>
+          <SearchBar />
+          <SearchResults />
+        </div>
+        <Playlist />
+      </div>
+    </>
   );
 }
 
